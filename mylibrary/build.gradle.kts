bdd73696c44d4fe4de00570ce8d4c7ce47bcb739
Niladree Bhattacharjee <niladreeb@gmail.com>
2025-03-28 22:51:05 +0530

    Publishing setup and workflow

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -40,4 +41,29 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                //get the file pointing to the aar file
+                val file = layout.buildDirectory.file("outputs/aar/${project.name}-release.aar")
+                artifact(file)
+            }
+            groupId = "com.nilutopia"
+            artifactId = "mylibrary"
+            version = "0.0.1"
+        }
+    }
+    repositories {
+        maven {
+            name = "github"
+            url = uri("https://maven.pkg.github.com/NilUtopia/JokeMe")
+            credentials {
+                username = System.getenv("GITHUB_USER")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
