a2466f6452b57fa17a513f715ed0bee112f635fa
Niladree Bhattacharjee <niladreeb@gmail.com>
2025-04-25 19:11:04 +0100

    Push pipeline for dokka

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -2,11 +2,12 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("maven-publish")
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 android {
     namespace = "com.nilutopia.mylibrary"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 28
@@ -35,7 +36,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
