ebb0852d3a8fcc55fae6c516a967c3767a968c89
Niladree Bhattacharjee <niladreeb@gmail.com>
2025-04-25 21:11:58 +0100

    Fixed test

--- app/src/test/java/com/demo/jokeme/ExampleUnitTest.kt
+++ app/src/test/java/com/demo/jokeme/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.demo.jokeme
 
-import org.junit.jupiter.api.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
